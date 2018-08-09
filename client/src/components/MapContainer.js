114d7ad1d5bb99c4494933441bfc9fe95da82f98
David Borah <dborah87@gmail.com>
2018-08-09 19:42:31 -0400

    Touched in Logo, Map, and MapContainer

---
+++ client/src/components/MapContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MapContainer extends Component {
+    render() {
+        return(
+            <div>
+                Map Container
+            </div>
+        );
+    }
+}
+
+export default MapContainer;
