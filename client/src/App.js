b208cdb31c1835e22e0ef71af0bf4c5e4fc91299
David Borah <dborah87@gmail.com>
2018-08-13 20:17:14 -0400

    Setup React Router with all of the pages mapped to their respective routes. Utilized Switch to disallow someone from navigating to an unknown page

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
-//temporary for testing
 import Find from './pages/Find';
 import Park from './pages/Park';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Logo from './components/Logo';
-import Map from './components/Map';
-import MapContainer from './components/MapContainer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Find />
-        <Park />
-        <Signup />
-        <Login />
-        <Home />
         <Logo />
-        <Map />
-        <MapContainer />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/park" component={Park} />
+          <Route path="/find" component={Find} />
+          <Redirect to="/" />
+        </Switch>
       </div>
     );
   }
